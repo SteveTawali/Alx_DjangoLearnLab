fd27c7dcd8fe591235fa8571adb054f495e94d4e
Steven Gulu <stevenkatawali@gmail.com>
2024-11-12 08:41:16 +0300

    Made changes to urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- django-models/bookshelf/delete.md
+++
@@ -1,8 +0,0 @@
- ### Delete Operation
-
-```python
-from bookshelf.models import Book
-retrieved_book.delete()
-all_books = Book.objects.all()
-print(all_books)
-
