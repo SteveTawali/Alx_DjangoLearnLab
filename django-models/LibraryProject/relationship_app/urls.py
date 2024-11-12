bae79963b86419c19b355dce03bd09de4d456fd8
Steven Gulu <stevenkatawali@gmail.com>
2024-11-12 08:49:01 +0300

    urls.py added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import list_books
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
