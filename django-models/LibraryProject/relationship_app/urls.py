6fa48c1c7ae3d08bf1f02c048fd1fae93e8e1f62
Steven Gulu <stevenkatawali@gmail.com>
2024-11-12 09:03:45 +0300

    configured url patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import list_books
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+]
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
