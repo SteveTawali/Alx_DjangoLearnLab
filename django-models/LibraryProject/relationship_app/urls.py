4f10c0b759b31fbaa6a562782dcad164d739f4a0
Steven Gulu <stevenkatawali@gmail.com>
2024-11-12 09:55:50 +0300

    Add urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path('add-book/', views.add_book, name='add_book'),
-    path('edit-book/<int:book_id>/', views.edit_book, name='edit_book'),
-    path('delete-book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
