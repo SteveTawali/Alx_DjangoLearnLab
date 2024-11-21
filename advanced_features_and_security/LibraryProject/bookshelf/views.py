579db32c13326d8f00d4c8c596f8d2a9bafb780b
Steven Gulu <stevenkatawali@gmail.com>
2024-11-21 21:20:54 +0300

    Add views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 
-@permission_required('app_name.can_edit', raise_exception=True)
-def edit_view(request, id):
-    # Edit logic
-    pass
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    # Replace this with your logic to fetch and display books
+    return render(request, 'bookshelf/book_list.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def books(request):
+    # Replace this with your logic to fetch a specific book or handle books
+    return HttpResponse("Books endpoint")
