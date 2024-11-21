a73d2dc9026b763db913956489bb172bcc0d6955
Steven Gulu <stevenkatawali@gmail.com>
2024-11-21 21:38:07 +0300

    views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -11,3 +11,20 @@ def book_list(request):
 def books(request):
     # Replace this with your logic to fetch a specific book or handle books
     return HttpResponse("Books endpoint")
+
+
+from django.shortcuts import render
+from .forms import ExampleForm
+
+def example_view(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Handle valid form submission
+            print(form.cleaned_data)
+            return render(request, "form_success.html")
+    else:
+        form = ExampleForm()
+
+    return render(request, "example_form.html", {"form": form})
+
