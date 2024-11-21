27d51d8f7978ff2552e9eb51da1af2a92e829f00
Steven Gulu <stevenkatawali@gmail.com>
2024-11-21 21:35:47 +0300

    Add Forms

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
-class BookForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    author = forms.CharField(max_length=255)
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, label="Name")
+    email = forms.EmailField(required=True, label="Email")
+    message = forms.CharField(
+        widget=forms.Textarea,
+        max_length=500,
+        required=True,
+        label="Message"
+    )
