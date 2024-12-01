01d78165c9e2cf4ca92664e87bb59352444cb5fe
SteveTawali <Stevenkatawali@gmail.com>
2024-12-01 07:54:01 +0300

    Add filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -13,7 +13,7 @@
 
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
