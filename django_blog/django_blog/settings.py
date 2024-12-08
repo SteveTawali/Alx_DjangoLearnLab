93061311e24aff3c7c6aadb0ad40d508a62ff3e5
SteveTawali <Stevenkatawali@gmail.com>
2024-12-08 15:39:08 +0300

    Updated settings.py with Mysql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',  # Or the address of your database server
+        'PORT': '3306',       # Default MySQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
