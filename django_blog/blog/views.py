56d7382ec83c5111b8b9ae21a7b97b3e15732b0a
SteveTawali <Stevenkatawali@gmail.com>
2024-12-08 21:31:28 +0300

    Made changes to views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import PostForm  # Ensure CustomUserUpdateForm is imported
+from .forms import PostForm, CommentForm  # Ensure CommentForm is imported
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .models import Comment, Post
-from .forms import CommentForm
+from .models import Post, Comment
 
+# Comment Create View
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
@@ -24,83 +20,38 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['post_id']})
 
-class CommentUpdateView(LoginRequiredMixin, UpdateView):
+# Comment Update View
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
     template_name = 'comment_form.html'
 
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
     def get_success_url(self):
         return reverse_lazy('post_detail', kwargs={'pk': self.object.post_id})
 
     def get_queryset(self):
         return Comment.objects.filter(author=self.request.user)
 
-class CommentDeleteView(LoginRequiredMixin, DeleteView):
+# Comment Delete View
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = 'comment_confirm_delete.html'
 
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
     def get_success_url(self):
         return reverse_lazy('post_detail', kwargs={'pk': self.object.post_id})
 
     def get_queryset(self):
         return Comment.objects.filter(author=self.request.user)
-
 
 @login_required
 def profile_view(request):
     if request.method == 'POST':
-        form = PostForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')
-    else:
-        form = PostForm(instance=request.user)
-
-    return render(request, 'registration/profile.html', {'form': form})
-
-
-# ListView to display all blog posts
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
-
-
-# DetailView to show a single blog post
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
-
-
-# CreateView for creating new blog posts
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-
-# UpdateView for editing existing posts
-class PostUpdateView(LoginRequiredMixin, UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-
-    def form_valid(self, form):
-        if self.request.user != form.instance.author:
-            raise PermissionError("You do not have permission to edit this post.")
-        return super().form_valid(form)
-
-
-# DeleteView for deleting posts
-class PostDeleteView(LoginRequiredMixin, DeleteView):
-    model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post_list')
-
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
+       form = PostForm(request.POST, instance=request)
