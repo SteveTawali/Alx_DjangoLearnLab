dfafdd31de7ae2f8ea4a1a2397f589d70a8256ec
SteveTawali <Stevenkatawali@gmail.com>
2024-12-08 20:26:05 +0300

    Add different templates and views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,3 +8,15 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("profile/", profile, name="profile"),
 ]
+
+
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
+urlpatterns = [
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -18,4 +18,55 @@ def profile_view(request):
         form = CustomUserUpdateForm(instance=request.user)
 
     return render(request, 'registration/profile.html', {'form': form})
+
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
+
+# ListView to display all blog posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+# DetailView to show a single blog post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+# CreateView for creating new blog posts
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Automatically set the current user as the author
+        return super().form_valid(form)
+
+# UpdateView for editing existing posts
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        # Ensure that only the post author can edit
+        if self.request.user != form.instance.author:
+            raise PermissionError("You do not have permission to edit this post.")
+        return super().form_valid(form)
+
+# DeleteView for deleting posts
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+
+    def get_queryset(self):
+        # Ensure that only the post author can delete it
+        return Post.objects.filter(author=self.request.user)
 # Create your views here.
