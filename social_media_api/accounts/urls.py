bdde5c033a9ec8a99bccac4e7f07b846f99c12e9
SteveTawali <Stevenkatawali@gmail.com>
2024-12-24 19:03:21 +0300

    Changes to serializers.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -3,4 +3,6 @@ from .views import RegisterView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),  # Add the login route here
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
